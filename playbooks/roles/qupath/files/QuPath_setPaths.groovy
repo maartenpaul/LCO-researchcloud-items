fdabebfb2d00dfa15eb5c2d36cf9e13d43d51519
Maarten Paul <maarten.paul@gmail.com>
2026-01-27 15:34:14 +0100

    refactor: streamline QuPath setPaths script and improve error handling in main.yml

--- playbooks/roles/qupath/files/QuPath_setPaths.groovy
+++ playbooks/roles/qupath/files/QuPath_setPaths.groovy
@@ -1,18 +1,14 @@
-import qupath.lib.gui.prefs.QuPathPrefs
+import qupath.lib.gui.prefs.PathPrefs
 import javafx.application.Platform
 
-// We use the path where we extracted the Zenodo files
+// Update this to the path where your extensions are
 def extensionPath = "/opt/QuPath_Common_Data/extensions"
-def file = new File(extensionPath)
 
-if (file.exists()) {
-    // Set the extension directory in QuPath's internal preferences
-    QuPathPrefs.extensionDirectoryProperty().set(file.getAbsolutePath())
-    println "QuPath extension path set to: " + file.getAbsolutePath()
-} else {
-    println "Warning: Extension path not found at " + extensionPath
+try {
+    PathPrefs.getExtensionDirectoryProperty().set(extensionPath)
+    println "Successfully set extension path to: " + extensionPath
+} catch (Exception e) {
+    println "Error setting path: " + e.getMessage()
 }
 
-// Ensure preferences are flushed/saved
-// QuPath usually does this on exit, but calling a preference check helps
 Platform.exit()
